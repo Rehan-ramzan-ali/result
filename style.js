59c95e289db8c8971df06625ca95516880b4db0c
Rehan-ramzan-ali <ra6313002@gmail.com>
2025-07-25 19:51:20 +0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,39 @@
+
+var marks= Number(prompt(" enter your marks"))
+
+
+if ( marks >= 90 && marks <=100)
+    {
+        console.log("Grade A1")
+    }
+
+    else if( marks >= 80 && marks <=90){
+        console.log("Grade A")
+    }
+
+    else if( marks >= 70 && marks <=80){
+        console.log("Grade B")
+    }
+
+    else if( marks >= 60 && marks <=70){
+        console.log("Grade C")
+    }
+
+    else if( marks >= 50 && marks <=60){
+        console.log("Grade D")
+    }
+
+    else if( marks < 50 ){
+        console.log("Failed")
+    }
+      else if( marks > 100){
+        console.log("please enter a number between 1 to 100")
+    }
+
+
+
+
+    else {
+        console.log
+        ("sorry tryagin")
+    }
